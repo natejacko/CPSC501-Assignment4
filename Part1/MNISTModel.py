b7071594842ef386cabcc624743f48bb9a4223e6
natejacko <njacko@shaw.ca>
2019-11-26 22:18:49 -0700

    Part1 model with 99+% train 98+% test

---
+++ Part1/MNISTModel.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+print("--Get data--")
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+print("--Process data--")
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+print("--Make model--")
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(256, activation='relu'),
+  tf.keras.layers.Dropout(0.1),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+model.compile(optimizer='Adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+print("--Fit model--")
+model.fit(x_train, y_train, batch_size=128, epochs=10, verbose=2)
+
+print("--Evaluate model--")
+model_loss, model_acc = model.evaluate(x_test,  y_test, verbose=2)
+print(f"Model Loss:    {model_loss:.2f}")
+print(f"Model Accuray: {model_acc*100:.1f}%")
+
+#Save Model
+model.save("./Part1/MNIST.h5")
